5588ebd988fe9bb03c154528a2193bc901c2f85a
Dario La Tegola <dariolategola@gmail.com>
2022-02-09 15:37:14 +0100

    Pratica con immagine

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const app = new Vue(
         el: "#root",
         data: {
             message: '"Da un grande potere derivano grandi responsabilità"',
+            image: "img/asm-50.jpg"
         }
     }
 
