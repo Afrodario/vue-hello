a3748dfd1f0bc46c544c842808a3c8ec0c9c47ca
Dario La Tegola <dariolategola@gmail.com>
2022-02-09 16:30:14 +0100

    Un po' di personalizzazione

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ const app = new Vue(
         el: "#root",
         data: {
             message: '"Da un grande potere derivano grandi responsabilità"',
-            image: "img/asm-50.jpg"
+            image: "img/asm-50.jpg",
+            issue: "The Amazing Spider-Man (Vol. 1) #050",
+            releaseDate: "11 Aprile 1967",
+            coverDate: "Luglio 1967",
+            writer: "Stan Lee",
+            penciler: "John Romita Sr.",
+            inker: "Mickey Demeo",
+            letterer: "Sam Rosen",
+            editor: "Stan Lee" 
         }
     }
 
